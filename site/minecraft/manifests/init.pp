28cc0b193dcb20ab2af55b5c8e626bb80fa38cda
tonysla37 <69595140+tonysla37@users.noreply.github.com>
2022-07-20 16:24:35 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -7,6 +7,16 @@ class minecraft {
     source => 'https://launcher.mojang.com/v1/objects/e00c4052dac1d59a1188b2aa9d5a87113aaf1122/server.jar',
     before => Service['minecraft'],
   }
+  file {'/tmp/jdk-18_linux-x64_bin.rpm':
+    ensure => file,
+    source => 'https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.rpm',
+  }
+  package { 'jdk-18_linux-x64_bin':
+    ensure => present,
+    provider => 'rpm',
+    source => "/tmp/jdk-18_linux-x64_bin.rpm",
+    require => File["/tmp/jdk-18_linux-x64_bin.rpm"],
+  }
   package {'java':
     ensure => present,
   }
